33c9b51331c34cda3506ac38c47882a4f7af2bfc
RezzedUp <RezzedUp@users.noreply.github.com>
2023-01-04 16:19:07 -0600

    Update copyright. Bump plugin version.

--- src/main/java/community/leaf/survival/concretemixer/ConcreteMixerPlugin.java
+++ src/main/java/community/leaf/survival/concretemixer/ConcreteMixerPlugin.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2022, RezzedUp and Contributors <https://github.com/LeafCommunity/ConcreteMixer>
+ * Copyright © 2022-2023, RezzedUp and Contributors <https://github.com/LeafCommunity/ConcreteMixer>
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
